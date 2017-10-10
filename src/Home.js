26b83fe757de9c661a6b98b54bc7b64bee581659
claudia <clou.7loymed@gmail.com>
2017-10-10 16:54:07 -0500

    home

--- src/Home.js
+++ src/Home.js
@@ -1,39 +1,33 @@
 import React, {Component} from 'react';
 import {
-	BrowserRouter,
-	Route,
-	Switch,
 	NavLink,
-	Redirect
 } from 'react-router-dom'
+import './Home.css'
 
 const Home = ( {model} ) => {
 
 	return (
 		<div className="home">
-		<div className="container">
-			<header>
-				<div className="container header">
-					<div class="row">
-						<div className="col-xs-12 text-center">
-							<img src="image/gio.png" alt=""/>
-						</div>
+			<div className="header">
+				<center>
+					<img src="./image/gio.png" className="logo text-center"/>
+				</center>
+			
+			<div className="container">
+				<div className="row  btns">
+					<div className="col-xs-6 col-md-6">
+						<NavLink to={"/signup"} className="btn btn-lyft btn-lg btn-block btn-signUp">
+							Log In
+						</NavLink>
 					</div>
-				</div>
-			</header>
-			<div>
-			<div className="col-md-6 col-xs-6">
-				<NavLink to={"/signup"} className="btn btn-lyft btn-lg btn-block">
-					Log in
-				</NavLink>
+					<div className="col-xs-6 col-md-6">
+						<NavLink to={"/signup"} className="botonPink btn btn-lyft btn-lg btn-block btn-signUp">
+							Sign Up
+						</NavLink>
+					</div>
+				</div>	
 			</div>
-			<div className="col-md-6 col-xs-6">
-				<NavLink to={"/signup"} className="btn botonPink btn-lg btn-block">
-					Sign Up
-				</NavLink>
-			</div>
-			</div>
-		</div>
+			</div>	
 		</div>
 	);
 }
